d22e7e593642cada24102645f95109c6b2dee4fb
Andris <andris_viinkalns@inbox.lv>
2017-10-04 16:10:00 +0100

    Building board for game

--- src/com/vkalns/Game.java
+++ src/com/vkalns/Game.java
@@ -1,19 +1,48 @@
 package com.vkalns;
 
+import com.vkalns.model.Board;
 import com.vkalns.model.Move;
 
+import java.io.Console;
 import java.util.Stack;
 
 public class Game
 {
     Stack<Move> movesTaken = new Stack<>();
+    Board board = new Board();
 
     public void start()
     {
+        String [][] board  = {
+                {"w"," ","w"," ","w"," ","w"," "},
+                {" ","w"," ","w"," ","w"," ","w"},
+                {"w"," ","w"," ","w"," ","w"," "},
+                {" ","*"," ","*"," ","*"," ","*"},
+                {"*"," ","*"," ","*"," ","*"," "},
+                {" ","b"," ","b"," ","b"," ","b"},
+                {"b"," ","b"," ","b"," ","b"," "},
+                {"_","b","_","b","_","b","_","b"}};
+
+        for(int y=1;y<9;y++ )
+        {
+            System.out.print(y+"|");
+            for(int x=1;x<9;x++ )
+            {
+            System.out.print(board[y-1][x-1]+"|");
+            if (x==8)
+            {
+                System.out.println("");
+            }
+            }
+            if (y==8)
+            {
+                System.out.println("  1 2 3 4 5 6 7 8");
+            }
+        }
+    }
         //here all the methods will be called to play the game till the result
         //This method will be called in Main class
 
-    }
 
     public void undoMove()
     {
--- src/com/vkalns/model/Board.java
+++ src/com/vkalns/model/Board.java
@@ -7,9 +7,17 @@ public class Board
     private ArrayList<Piece> myPieces = new ArrayList<>();
     private ArrayList<Piece> aiPieces = new ArrayList<>();
 
+
+
+    public Board  (){};
+
     public Board(ArrayList<Piece> myPieces, ArrayList<Piece> aiPieces)
     {
         this.myPieces = myPieces;
         this.aiPieces = aiPieces;
     }
+
+
+
+
 }
