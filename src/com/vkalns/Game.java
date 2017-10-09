b8d05b398d2daebabb1d903311c15a39491ecad7
Andris <andris_viinkalns@inbox.lv>
2017-10-09 11:32:09 +0100

    Lot's of updates for board + Prompter class. Deleted some unnesecary code

--- src/com/vkalns/Game.java
+++ src/com/vkalns/Game.java
@@ -2,6 +2,7 @@ package com.vkalns;
 
 import com.vkalns.model.Board;
 import com.vkalns.model.Move;
+import com.vkalns.model.Prompter;
 
 import java.io.Console;
 import java.util.Stack;
@@ -9,36 +10,16 @@ import java.util.Stack;
 public class Game
 {
     Stack<Move> movesTaken = new Stack<>();
+    Stack<Move> movesUndo = new Stack<>();
     Board board = new Board();
+    Prompter prompter = new Prompter();
 
     public void start()
     {
-        String [][] board  = {
-                {"w"," ","w"," ","w"," ","w"," "},
-                {" ","w"," ","w"," ","w"," ","w"},
-                {"w"," ","w"," ","w"," ","w"," "},
-                {" ","*"," ","*"," ","*"," ","*"},
-                {"*"," ","*"," ","*"," ","*"," "},
-                {" ","b"," ","b"," ","b"," ","b"},
-                {"b"," ","b"," ","b"," ","b"," "},
-                {"_","b","_","b","_","b","_","b"}};
-
-        for(int y=1;y<9;y++ )
-        {
-            System.out.print(y+"|");
-            for(int x=1;x<9;x++ )
-            {
-            System.out.print(board[y-1][x-1]+"|");
-            if (x==8)
-            {
-                System.out.println("");
-            }
-            }
-            if (y==8)
-            {
-                System.out.println("  1 2 3 4 5 6 7 8");
-            }
-        }
+        board.drawBoard();
+        movesTaken.push(new Move(prompter.askForMove(),board));
+        System.out.println("Moving a piece from: "+movesTaken.peek().getStartingPos()+
+                " to "+movesTaken.peek().getTargetPos());
     }
         //here all the methods will be called to play the game till the result
         //This method will be called in Main class
@@ -47,6 +28,11 @@ public class Game
     public void undoMove()
     {
         //TODO: remove last element from movesTaken Stack
+        if(!movesTaken.isEmpty())
+        {//takes move off and puts it in another undo move stack
+            //movesTaken.peek().run();
+            movesUndo.push(movesTaken.pop());
+        }
     }
 
     public void end()
--- src/com/vkalns/model/Position.java
+++ src/com/vkalns/model/Position.java
@@ -1,33 +1,10 @@
 package com.vkalns.model;
 
-public class Position
-{
-    private int xPos;
-    private int yPos;
+import java.util.Stack;
 
-    //Constructors
-    public Position (){};
+public class Position {
 
-    public Position(int xPos, int yPos) {
-        this.xPos = xPos;
-        this.yPos = yPos;
-    }
-
-    //getters and setters
-    public int getxPos() {
-        return xPos;
-    }
-
-    public void setxPos(int xPos) {
-        this.xPos = xPos;
-    }
-
-    public int getyPos() {
-        return yPos;
-    }
-
-    public void setyPos(int yPos) {
-        this.yPos = yPos;
-    }
 }
+
+
 
